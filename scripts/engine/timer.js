39ef7ff046e4037b92e9795cbcb1719b6679bad1
krobison10 <kylerrobison10@outlook.com>
2022-12-15 15:34:22 -0800

    structure changes

---
+++ scripts/engine/timer.js
@@ -0,0 +1,19 @@
+"use strict";
+
+class Timer {
+    constructor() {
+        this.gameTime = 0;
+        this.maxStep = 0.05;
+        this.lastTimestamp = 0;
+    };
+
+    tick() {
+        const current = Date.now();
+        const delta = (current - this.lastTimestamp) / 1000;
+        this.lastTimestamp = current;
+
+        const gameDelta = Math.min(delta, this.maxStep);
+        this.gameTime += gameDelta;
+        return gameDelta;
+    };
+};
