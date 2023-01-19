d2665b074b3f937e6aacf55808a0851ed16bb956
krobison10 <kylerrobison10@outlook.com>
2023-01-19 11:57:20 -0800

    Minor tweaks

--- scripts/hotbar.js
+++ scripts/hotbar.js
@@ -1,39 +1,30 @@
- class Hotbar extends Entities {
-                    constructor(player) {
-                        super(new Vec2(0, 0), new Dimension(0, 0));
-                        this.agent = agent;
-                        this.width = 40;
-                        this.height = 20;
-                        this.offset = this.player.size.w - this.width;
-                        this.innerPadding = 5;
-                    }
+ class Hotbar extends Entity {
+    constructor() {
+        super(new Vec2(10, 10), new Dimension(40, 40));
+        this.innerPadding = 5;
+    }
                 
-                    update() {
-                        // update the state of the hotbar
-                   //this.selectedItem = this.agent.getSelectedItem();
-                    }
+    update() {
+        //update the state
+        //this.selectedItem = this.agent.getSelectedItem();
+    }
                 
-                    draw(ctx) {
-                        let screenPos = this.agent.getScreenPos();
-                
-                        ctx.fillStyle = rgba(50, 50, 50, 0.7);
-                        ctx.fillRect(screenPos.x + this.offset / 2 + this.innerPadding,
-                            screenPos.y + this.agent.size.h + this.innerPadding,
-                            this.width - 2 * this.innerPadding, this.height - 2 * this.innerPadding);
-                
-                        // Draw the items in the hotbar
-                        for(let i = 0; i < this.agent.hotbar.length; i++) {
-                            let item = this.agent.hotbar[i];
-                            let x = screenPos.x + this.offset / 2 + (i * this.width / this.agent.hotbar.length);
-                            let y = screenPos.y + this.agent.size.h;
-                            ctx.drawImage(item.image, x, y, this.width / this.agent.hotbar.length, this.height);
-                        }
+    draw(ctx) {
+        ctx.fillStyle = rgba(50, 50, 50, 0.7);
+        ctx.fillRect(this.pos.x, this.pos.y, this.size.w, this.size.h);
+
+        // // Draw the items in the hotbar, hotbar not yet implemented in player
+        // for(let i = 0; i < this.agent.hotbar.length; i++) {
+        //     let item = this.agent.hotbar[i];
+        //     let x = this.pos.x + this.innerPadding;
+        //     let y = this.pos.y + this.innerPadding;
+        //     ctx.drawImage(item.image, x, y, this.size.w - 2 * this.innerPadding, this.size.h - 2 * this.innerPadding);
+        // }
                
-                        ctx.strokeStyle = rgba(40, 40, 40, 1);
-                        ctx.lineWidth = '2.5';
+        ctx.strokeStyle = rgba(40, 40, 40, 1);
+        ctx.lineWidth = '2.5';
                 
-                        ctx.strokeRect(screenPos.x + this.offset / 2, screenPos.y + this.agent.size.h,
-                            this.width, this.height);
-                    }
-                }
+        ctx.strokeRect(this.pos.x, this.pos.y, this.size.w, this.size.h);
+    }
+}
 
