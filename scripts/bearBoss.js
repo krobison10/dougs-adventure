6ff01a11fc21bc31a867b555e45d6afc4f72d833
alay22405 <77366927+alay22405@users.noreply.github.com>
2023-01-29 19:57:04 -0800

    Update bearBoss.js

--- scripts/bearBoss.js
+++ scripts/bearBoss.js
@@ -4,119 +4,109 @@
  * @author Alay Kidane
  *
  */
+'use strict';
+
 class BearBoss extends Enemy {
-    /**
-     * Creates a new bear boss that represents a stationary, potentially collidable object in came
-     * @param {Vec2} pos the position of the object in the game world.
-     * @param {Dimension} size the size of the object in the game world.
-     * @param {HTMLImageElement} spritesheet the sprite or spritesheet.
-     * @param {boolean} collidable represents whether other entities will collide with the obstacle.
-     * @param {LightSource} lightSource to add to the object
-     * @param {Vec2} sheetPos the position of the sprite within the spritesheet.
-     * @param {Dimension} sheetSize the size of the sprite in the spritesheet.
-     */
-    
     constructor(pos, spritesheet, size, spritePadding, damage, hitPoints) {
         super(pos, spritesheet, size, spritePadding, damage, hitPoints);
 
-        this.animations = [];
-        this.maxHitPoints = hitPoints;
-        this.hitPoints = hitPoints;
-        this.damage = damage;
-        this.speed = 75;
-        this.velocity = new Vec2(0, -this.speed);
-        this.direction = "up";
+        // max hitpoints and damage of the boss
+        this.maxHitPoints = 500;
+        this.damage = 50;
+
+        // boss's movement speed
+        this.speed = 100;
+        this.velocity = new Vec2(0, 0);
+
+        // boss's collision bounding box
         this.boundingBox = Character.createBB(this.pos, this.size, this.spritePadding);
 
-
- //create the special attack animation
-
- // this.specialAttackAnimation = new Animator(this.spritesheet, 0, 4* this.size.h,
-//         this.size.w, this.size.h,
-//         5, 0.1, 0, false, true);
-
-
-        // Create animations for different directions
-        this.animations["up"] = new Animator(this.spritesheet, 0, 0, this.size.w, this.size.h, 3, 0.1, 0, false, true);
-        this.animations["down"] = new Animator(this.spritesheet, 0, this.size.h, this.size.w, this.size.h, 3, 0.1, 0, false, true);
-        this.animations["left"] = new Animator(this.spritesheet, 0, this.size.h * 2, this.size.w, this.size.h, 3, 0.1, 0, false, true);
-        this.animations["right"] = new Animator(this.spritesheet, 0, this.size.h * 3, this.size.w, this.size.h, 3, 0.1, 0, false, true);
+        // animations for the boss
+        this.animations = {
+            idle: new Animator(this.spritesheet, 0, 0, this.size.w, this.size.h, 1, 1, 0, false, true),
+            moveUp: new Animator(this.spritesheet, 0, this.size.h, this.size.w, this.size.h, 3, 0.2, 0, false, true),
+            moveDown: new Animator(this.spritesheet, 0, this.size.h * 2, this.size.w, this.size.h, 3, 0.2, 0, false, true),
+            moveLeft: new Animator(this.spritesheet, 0, this.size.h * 3, this.size.w, this.size.h, 3, 0.2, 0, false, true),
+            moveRight: new Animator(this.spritesheet, 0, this.size.h * 4, this.size.w, this.size.h, 3, 0.2, 0, false, true),
+            attack: new Animator(this.spritesheet, 0, this.size.h * 5, this.size.w, this.size.h, 3, 0.2, 0, false, true),
+        };
     }
+
+   
+    update() {
+// call custom move function
+this.move();
+
+        // change direction and velocity randomly
+        if (Math.random() < 0.05) {
+            this.velocity.x = (Math.random() - 0.5) * 5;
+            this.velocity.y = (Math.random() - 0.5) * 5;
+        }
+    
+        // check if the bear boss is dead
+        if (this.hitPoints <= 0) {
+            this.velocity.x = this.velocity.y = 0;
+            return;
+        }
+//new
+   // update direction based on velocity
+   if (this.velocity.x < 0) {
+    this.drawAnim(gameEngine.ctx, this.animations.moveLeft);
+} else if (this.velocity.x > 0) {
+    this.drawAnim(gameEngine.ctx, this.animations.moveRight);
+} else if (this.velocity.y < 0) {
+    this.drawAnim(gameEngine.ctx, this.animations.moveUp);
+} else if (this.velocity.y > 0) {
+    this.drawAnim(gameEngine.ctx, this.animations.moveDown);
+} else {
+    this.drawAnim(gameEngine.ctx, this.animations.idle);
+}
 
     
-
-    update() {
-        // Move the boss in the direction it is facing
-        this.pos.x += this.velocity.x * gameEngine.clockTick;
-        this.pos.y += this.velocity.y * gameEngine.clockTick;
-
-        // Check for collision with boundaries
-        //left
-        if (this.pos.x < 0) {
-            //speed down
-            this.velocity.y = this.speed;
-            this.pos.x=0;
-            this.velocity.x = 0;
-            this.direction = "up";
+        /**
+         * Check for collision, we do two separate checks
+         */
+        const collisionLat = this.checkCollide("lateral");
+        const collisionVert = this.checkCollide("vertical");
+        if (!collisionLat) {
+            this.pos.x += this.velocity.x * gameEngine.clockTick;
         }
-        //to his up
-        else if (this.pos.x + this.size.w > 300) {
-  
-            this.velocity.y = -this.speed;
-            this.velocity.x = 0;
-            this.pos.x=300-this.size.w;
-            this.direction = "right";
+        if (!collisionVert) {
+            this.pos.y += this.velocity.y * gameEngine.clockTick;
         }
-        else if (this.pos.y < 0) {
-            this.velocity.y = 0;
-            this.pos.y=0;
-            this.velocity.x = -this.speed;
-            this.direction = "down";
-        }
-
-        //to his left
-        else if(this.pos.y + this.size.h >= 300) {
-            this.velocity.y = 0;
-            this.pos.y=300 - this.size.h;
-            this.velocity.x = this.speed;
-            this.direction = "left";
-        }
-
+    
+        // update bounding box
         this.boundingBox = Character.createBB(this.pos, this.size, this.spritePadding);
     }
-
-    draw(ctx) {
-        // Draw the boss facing the direction it is moving
-        this.drawAnim(ctx, this.animations[this.direction]);
+    
+    
+/*
+calculate the angle of the velocity vector and 
+set the animator based on that angle
+*/
+move() {
+    // Change direction and velocity randomly
+    if(Math.random() < 0.5) {
+        this.velocity.x = (Math.random() - 0.1) * this.speed;
+        this.velocity.y = (Math.random() - 0.1) * this.speed;
     }
-
-    // Custom move function that allows the boss to move in a specific direction
-    move(direction) {
-        this.direction = direction;
-        switch(direction) {
-            case "up":
-                this.velocity.x = 0;
-                this.velocity.y = -this.speed;
-                break;
-            case "down":
-                this.velocity.x = 0;
-                this.velocity.y = this.speed;
-                break;
-            case "left":
-                this.velocity.x = -this.speed;
-                this.velocity.y = 0;
-                break;
-            case "right":
-                this.velocity.x = this.speed;
-                this.velocity.y = 0;
-                break;
-            default:
-                console.log("Invalid direction");
-        }
-        
+    
+    // Determine the direction the bear is facing based on its velocity
+    if (this.velocity.x < 0) {
+        this.currentAnimation = this.animations.moveRight;
+    } else if (this.velocity.x < 0) {
+        this.currentAnimation = this.animations.moveLeft;
+    } else if (this.velocity.y < 0) {
+        this.currentAnimation = this.animations.moveDown;
+    } else if (this.velocity.y < 0) {
+        this.currentAnimation = this.animations.moveUp;
     }
+}
+
+
     drawAnim(ctx, animator) {
         animator.drawFrame(gameEngine.clockTick, ctx, this.getScreenPos().x, this.getScreenPos().y, 1.5);
-    
-}
-}
+    }
+}    
+
+
