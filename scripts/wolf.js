f8b913ac9bf54006917dde6b094de8c9683b439e
RyanUWT <ryanmac1@uw.edu>
2023-03-04 11:38:33 -0800

    Added bite animation and collision with doug

--- scripts/wolf.js
+++ scripts/wolf.js
@@ -9,15 +9,23 @@ class Wolf extends Enemy {
     constructor(pos, spritesheet, size, spritePadding, damage, maxHitPoints) {
         super(pos, spritesheet, size, spritePadding, damage, maxHitPoints);
         this.animations = [];
+        this.idleAnimations = [];
 
         this.type = 'wolf';
+        this.animDisplace = {
+            lay: 4,
+            attack: 8
+        }
 
-        this.startX = this.pos.x;
-        this.startY = this.pos.y;
+        this.startPos = {
+            x: this.pos.x,
+            y: this.pos.y
+        }
+
         this.aggroRange = 200;
         this.enraged = false;
         this.pursuing = false;
-        this.hitPoints = maxHitPoints;
+        this.hitPoints = maxHitPoints; //Make sure it is instantiated before first update call
 
         this.playerRange = {
             xDif: doug.getCenter().x - this.getCenter().x,
@@ -42,17 +50,8 @@ class Wolf extends Enemy {
     update() {
         this.setSpeed();
         this.determineRange();
-        if(!doug.dead && this.playerRange.dist < this.aggroRange && this.playerRange.dist > 1) {
-            this.pursuing = true;
-        } else {
-            this.pursuing = false;
-        }
-        if(this.enraged || this.pursuing || this.startRange.dist > 1) {
-            this.move();
-        } else {
-            this.velocity.x = 0;
-            this.velocity.y = 0;
-        }
+        this.determineSize();
+        this.determineVelocity();
         const collisionLat = this.checkCollide("lateral");
         const collisionVert = this.checkCollide("vertical")
         if(!collisionLat) {
@@ -62,31 +61,19 @@ class Wolf extends Enemy {
             this.pos.y += this.velocity.y * gameEngine.clockTick;
         }
 
-        this.determineSize();
         this.boundingBox = Character.createBB(this.pos, this.size, this.spritePadding);
     }
 
     draw(ctx) {
-        const xAbsVel = Math.abs(this.velocity.x);
-        const yAbsVel = Math.abs(this.velocity.y);
-        if(xAbsVel > yAbsVel) { //Horizontal Frame
-            if (this.velocity.x < 0) { //Left
-                this.drawAnim(ctx, this.animations[3]);
-                this.directionMem = 3;
-            } else { //Right
-                this.drawAnim(ctx, this.animations[1]);
-                this.directionMem = 1;
-            }
-        } else if (xAbsVel < yAbsVel) { //Vertical Frame
-            if(this.velocity.y > 0) { //Down
-                this.drawAnim(ctx, this.animations[0]);
-                this.directionMem = 0;
-            } else { //Up
-                this.drawAnim(ctx, this.animations[2]);
-                this.directionMem = 2;
-            }
-        } else { //Idle Frame
-            this.drawAnim(ctx, this.animations[this.directionMem + 4]);
+        this.determineFrameDirection();
+        if(this.boundingBox.collide(doug.boundingBox)) { //Attack
+            this.drawAnim(ctx, this.animations[this.directionMem + this.animDisplace.attack]);
+        } 
+        else if(this.startRange.dist <= 1) { //Lay - currently just an idle standing animation
+            this.drawAnim(ctx, this.idleAnimations[this.directionMem]);
+        } 
+        else { //Walk
+            this.drawAnim(ctx, this.animations[this.directionMem]);
         }
 
         this.boundingBox.draw(ctx);
@@ -114,10 +101,43 @@ class Wolf extends Enemy {
         this.playerRange.dist =  Math.sqrt(Math.pow(this.playerRange.xDif, 2)
             + Math.pow(this.playerRange.yDif, 2));
         
-        this.startRange.xDif = this.startX - this.pos.x,
-        this.startRange.yDif = this.startY - this.pos.y,
+        this.startRange.xDif = this.startPos.x - this.pos.x,
+        this.startRange.yDif = this.startPos.y - this.pos.y,
         this.startRange.dist = Math.sqrt(Math.pow(this.startRange.xDif, 2)
                 + Math.pow(this.startRange.yDif, 2));
+    }
+
+    determineVelocity() {
+        if(!doug.dead && this.playerRange.dist < this.aggroRange) {
+            this.pursuing = true;
+        } else {
+            this.pursuing = false;
+        }
+        if(this.enraged || (this.pursuing && !this.boundingBox.collide(doug.boundingBox)) || (this.startRange.dist > 1 && !this.pursuing)) {
+            this.move();
+        } else {
+            this.velocity.x = 0;
+            this.velocity.y = 0;
+        }
+    }
+
+    determineFrameDirection() {
+        const xAbsVel = Math.abs(this.velocity.x);
+        const yAbsVel = Math.abs(this.velocity.y);
+        
+        if(xAbsVel > yAbsVel) { //Horizontal Frame
+            if (this.velocity.x < 0) { //Left
+                this.directionMem = 3;
+            } else { //Right
+                this.directionMem = 1;
+            }
+        } else if (xAbsVel < yAbsVel) { //Vertical Frame
+            if(this.velocity.y > 0) { //Down
+                this.directionMem = 0;
+            } else { //Up
+                this.directionMem = 2;
+            }
+        }
     }
 
     move() {
@@ -163,28 +183,100 @@ class Wolf extends Enemy {
             4, 0.2, 0, false, true);    //Frame Stats
         
         //Idle Down
-        this.animations[4] = new Animator(this.spritesheet, 
+        this.idleAnimations[0] = new Animator(this.spritesheet, 
             0, 128,                     //Start Positions
             32, 64,                     //Dimensions
             1, 0.2, 0, false, true);    //Frame Stats
 
         //Idle Right
-        this.animations[5] = new Animator(this.spritesheet, 
+        this.idleAnimations[1] = new Animator(this.spritesheet, 
             384, 96,                    //Start Positions
             64, 32,                     //Dimensions
             1, 0.2, 0, false, true);    //Frame Stats
 
         //Idle Up
-        this.animations[6] = new Animator(this.spritesheet, 
+        this.idleAnimations[2] = new Animator(this.spritesheet, 
             160, 128,                   //Start Positions
             32, 64,                     //Dimensions
             1, 0.2, 0, false, true);    //Frame Stats
 
-        //Walk Left
-        this.animations[7] = new Animator(this.spritesheet, 
+        //Idle Left
+        this.idleAnimations[3] = new Animator(this.spritesheet, 
             384, 288,                   //Start Positions
             64, 32,                     //Dimensions
+            1, 0.2, 0, false, true);    //Frame 
+            
+        //Lay Down
+        this.animations[4] = new Animator(this.spritesheet, 
+            0, 64,                     //Start Positions
+            32, 64,                     //Dimensions
+            4, 0.2, 0, false, false);    //Frame Stats
+
+        //Lay Right
+        this.animations[5] = new Animator(this.spritesheet, 
+            384, 0,                    //Start Positions
+            64, 32,                     //Dimensions
+            4, 0.2, 0, false, false);    //Frame Stats
+
+        //Lay Up
+        this.animations[6] = new Animator(this.spritesheet, 
+            160, 64,                   //Start Positions
+            32, 64,                     //Dimensions
+            4, 0.2, 0, false, false);    //Frame Stats
+
+        //Lay Left
+        this.animations[7] = new Animator(this.spritesheet, 
+            384, 192,                   //Start Positions
+            64, 32,                     //Dimensions
+            4, 0.2, 0, false, false);    //Frame Stats
+
+        //Idle Lay Down
+        this.idleAnimations[4] = new Animator(this.spritesheet, 
+            0, 64,                     //Start Positions
+            32, 64,                     //Dimensions
             1, 0.2, 0, false, true);    //Frame Stats
+
+        //Idle Lay Right
+        this.idleAnimations[5] = new Animator(this.spritesheet, 
+            384, 0,                    //Start Positions
+            64, 32,                     //Dimensions
+            1, 0.2, 0, false, true);    //Frame Stats
+
+        //Idle Lay Up
+        this.idleAnimations[6] = new Animator(this.spritesheet, 
+            160, 64,                   //Start Positions
+            32, 64,                     //Dimensions
+            1, 0.2, 0, false, true);    //Frame Stats
+
+        //Idle Lay Left
+        this.idleAnimations[7] = new Animator(this.spritesheet, 
+            384, 192,                   //Start Positions
+            64, 32,                     //Dimensions
+            1, 0.2, 0, false, false);    //Frame Stats
+
+        //Bite Down
+        this.animations[8] = new Animator(this.spritesheet, 
+            0, 256,                     //Start Positions
+            32, 64,                     //Dimensions
+            4, 0.2, 0, false, true);    //Frame Stats
+
+        //Bite Right
+        this.animations[9] = new Animator(this.spritesheet, 
+            384, 160,                    //Start Positions
+            64, 32,                     //Dimensions
+            4, 0.2, 0, false, true);    //Frame Stats
+
+        //Bite Up
+        this.animations[10] = new Animator(this.spritesheet, 
+            160, 256,                   //Start Positions
+            32, 64,                     //Dimensions
+            4, 0.2, 0, false, true);    //Frame Stats
+
+        //Bite Left
+        this.animations[11] = new Animator(this.spritesheet, 
+            384, 352,                   //Start Positions
+            64, 32,                     //Dimensions
+            4, 0.2, 0, false, true);    //Frame Stats
     }
 
     setSpeed() {
